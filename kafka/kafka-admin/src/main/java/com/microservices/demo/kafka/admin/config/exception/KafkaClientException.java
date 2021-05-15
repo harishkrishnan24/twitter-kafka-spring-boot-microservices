e6e57567765acdcdab87d36396585a9c225dff2f
Harish krishnan <harishkrishnan1993@gmail.com>
2021-05-15 16:27:42 +0530

    create kafka topics

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/exception/KafkaClientException.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.kafka.admin.config.exception;
+
+public class KafkaClientException extends RuntimeException{
+    public KafkaClientException() {
+    }
+
+    public KafkaClientException(String message) {
+        super(message);
+    }
+
+    public KafkaClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
