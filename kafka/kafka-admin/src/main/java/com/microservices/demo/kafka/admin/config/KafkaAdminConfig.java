27b8cb1fe36a3307b0602c9d4a3723dc8f50ef70
Harish krishnan <harishkrishnan1993@gmail.com>
2021-05-15 15:41:49 +0530

    add kafka admin client

---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/KafkaAdminConfig.java
@@ -0,0 +1,26 @@
+package com.microservices.demo.kafka.admin.config;
+
+import com.microservices.demo.config.KafkaConfigData;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+import java.util.Map;
+
+@Configuration
+@EnableRetry
+public class KafkaAdminConfig {
+
+    private final KafkaConfigData kafkaConfigData;
+
+    public KafkaAdminConfig(KafkaConfigData kafkaConfigData) {
+        this.kafkaConfigData = kafkaConfigData;
+    }
+
+    @Bean
+    public AdminClient adminClient() {
+        return AdminClient.create(Map.of(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers()));
+    }
+}
---
+++ kafka/kafka-admin/src/main/java/com/microservices/demo/kafka/admin/config/client/KafkaAdminClient.java
@@ -0,0 +1,27 @@
+package com.microservices.demo.kafka.admin.config.client;
+
+import com.microservices.demo.config.KafkaConfigData;
+import com.microservices.demo.config.RetryConfigData;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaAdminClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaAdminClient.class);
+
+    private final KafkaConfigData kafkaConfigData;
+    private final RetryConfigData retryConfigData;
+    private final AdminClient adminClient;
+    private final RetryTemplate retryTemplate;
+
+    public KafkaAdminClient(KafkaConfigData kafkaConfigData, RetryConfigData retryConfigData, AdminClient adminClient, RetryTemplate retryTemplate) {
+        this.kafkaConfigData = kafkaConfigData;
+        this.retryConfigData = retryConfigData;
+        this.adminClient = adminClient;
+        this.retryTemplate = retryTemplate;
+    }
+}
